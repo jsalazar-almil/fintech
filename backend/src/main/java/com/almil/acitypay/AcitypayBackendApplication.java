70eacf6bdff144f6251be168012db87fecce1619
Jose Salazar <jsalazar@almil.com.pe>
2026-06-27 14:41:36 -0500

    feat: despliega backend spring boot en kubernetes

---
+++ backend/src/main/java/com/almil/acitypay/AcitypayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.almil.acitypay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcitypayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcitypayBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/almil/acitypay/AcitypayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.almil.acitypay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcitypayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
